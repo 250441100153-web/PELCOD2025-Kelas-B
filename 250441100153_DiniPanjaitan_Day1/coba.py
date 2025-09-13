90a4a75e1654d697e27342c903cfb2b465974abc
250441100153-web <250441100153@student.trunojoyo.ac.id>
2025-09-13 20:40:34 +0700

    250441100153_DiniPanjaitan_Day1

---
+++ 250441100153_DiniPanjaitan_Day1/coba.py
@@ -0,0 +1,20 @@
+# a = 10
+# b = 5
+# c = a + b
+# d = a - b
+# e = a * b
+# f = a / b
+# g = a % b
+# print ("Hasil penjumlahan =", c)
+# print ("Hasil pengurangan =", d)
+# print ("Hasil perkalian =", e)
+# print ("Hasil pembagian =", f)
+# print ("Hasil sisa bagi =", g)
+
+nama = input ("Masukkan nama : ")
+umur = input ("Masukkan umur : ")
+buah = input ("Masukkan buah favorit : ")
+print("\n")
+print ("Nama saya adalah : " + nama)
+print ("Umur saya adalah : " + umur + " tahun")
+print ("Buah favorit saya adalah : " + buah)
