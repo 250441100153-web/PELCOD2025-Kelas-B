1debc723996739eee4b0c8251f99a5f4ba66a49d
250441100149_santaidasianturi_tugas1 <santaidasianturi@gmail.com>
2025-09-19 12:11:52 +0700

    250441100153_DiniPanjaitan

---
+++ 250441100153_DiniPanjaitan_Day1/tugas.py
@@ -0,0 +1,19 @@
+nama = "Dini Panjaitan"
+NIM  = 250441100153
+Semester = 1
+
+tugas = 85
+uts = 75
+uas = 90
+
+a = 10
+b = 5
+c = a + b
+
+nilai_akhir = (tugas * a) + (uts * b )+ (uas * c)
+
+print ("---nilai yang diperoleh---")
+print ("Nilai tugas :", tugas)
+print ("Nilai uts:", uts)
+print ("Nilai uas:", uas)
+print ("Nilai akhir dari tugas (30):", nilai_akhir)
